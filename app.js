42a733a579615f99712387d2951182100d86e877
gachalas3-lab <gachalas3@gmail.com>
2026-07-08 11:57:43 -0400

    Replaced reader.onload

--- app.js
+++ app.js
@@ -11,32 +11,51 @@ button.addEventListener("click", async () => {
 
     const reader = new FileReader();
 
-    reader.onload = async function () {
+reader.onload = async function () {
 
-        const typedArray = new Uint8Array(this.result);
+    const typedArray = new Uint8Array(this.result);
 
-        const pdf = await pdfjsLib.getDocument({
-            data: typedArray
-        }).promise;
+    const pdf = await pdfjsLib.getDocument({
+        data: typedArray
+    }).promise;
 
-        let allText = "";
+    let allText = "";
 
-        for (let page = 1; page <= pdf.numPages; page++) {
+    for (let page = 1; page <= pdf.numPages; page++) {
 
-            const pdfPage = await pdf.getPage(page);
+        const pdfPage = await pdf.getPage(page);
+        const textContent = await pdfPage.getTextContent();
 
-            const textContent = await pdfPage.getTextContent();
+        allText += textContent.items.map(item => item.str).join(" ");
+        allText += "\n";
+    }
 
-            const pageText = textContent.items
-                .map(item => item.str)
-                .join(" ");
+    const lines = allText.split(/\d{12}\s/);
 
-            allText += pageText + "\n\n";
-        }
+    let products = [];
 
-        document.getElementById("results").textContent = allText;
+    for (let i = 1; i < lines.length; i++) {
 
-    };
+        const piece = lines[i];
+
+        const upc = allText.match(/\d{12}/g)[i-1];
+
+        const name = piece.split(/\d+\sPK|\d+\sML|\d+\sL/)[0]
+            .trim()
+            .replace(/\s+/g," ");
+
+        products.push({
+            upc,
+            name
+        });
+    }
+
+    document.getElementById("results").innerHTML =
+        products.map(p => `<div>${p.upc} - ${p.name}</div>`).join("");
+
+    console.log(products);
+
+};
 
     reader.readAsArrayBuffer(file);
 
